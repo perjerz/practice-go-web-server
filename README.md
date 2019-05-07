e57479ad6b9baac91ff629ffc2b2dcd5e31c3b7b
perjerz3434 <perjerz3434@gmail.com>
2019-05-08 01:14:17 +0700

    add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practice Go Web Server
+
+### use standard lib
--- main.go
+++ main.go
@@ -10,16 +10,16 @@ import (
 )
 
 type Student struct {
-	Name string `json:"name"`
-	Age int `json:"age"`
+	Name  string  `json:"name"`
+	Age   int     `json:"age"`
 	Score float64 `json:"score"`
 }
 
 type Todo struct {
-	UserId int `json:"userId"`
-	Id int `json:"id"`
-	Title string `json:"title"`
-	Completed bool `json:"completed"`
+	UserId    int    `json:"userId"`
+	Id        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
 }
 
 func main() {
@@ -31,14 +31,14 @@ func main() {
 	r.HandleFunc("/encode", encode).Methods("GET")
 	r.HandleFunc("/marshal", marshal).Methods("GET")
 	r.HandleFunc("/fetch", fetchGoogle).Methods("GET")
-	r.HandleFunc("/student/{student}/todo/{todo}", func (w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/student/{student}/todo/{todo}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		s := vars["student"]
 		t := vars["todo"]
 		fmt.Fprintf(w, "student: %v, todo %v ", s, t)
 	}).Methods("GET")
 	r.Handle("/notfound", r.NotFoundHandler)
-	r.Handle("/static", http.StripPrefix("/static/",http.FileServer(http.Dir("/static/"))))
+	r.Handle("/static", http.StripPrefix("/static/", http.FileServer(http.Dir("/static/"))))
 
 	log.Printf("Listen on port: %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), r))
@@ -50,17 +50,18 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func decode(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case http.MethodPost: {
-		var s Student
-		decoder := json.NewDecoder(r.Body)
-		err := decoder.Decode(&s)
-		if err != nil {
-			http.Error(w, "Error", http.StatusBadRequest)
-			return
+	case http.MethodPost:
+		{
+			var s Student
+			decoder := json.NewDecoder(r.Body)
+			err := decoder.Decode(&s)
+			if err != nil {
+				http.Error(w, "Error", http.StatusBadRequest)
+				return
+			}
+			fmt.Println(s)
+			w.Write([]byte("Good job"))
 		}
-		fmt.Println(s)
-		w.Write([]byte("Good job"))
-	}
 	default:
 		w.Write([]byte("Method Not Allowed"))
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -69,21 +70,22 @@ func decode(w http.ResponseWriter, r *http.Request) {
 
 func unmarshal(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case http.MethodPost: {
-		var s Student
-		b, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+	case http.MethodPost:
+		{
+			var s Student
+			b, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			err = json.Unmarshal(b, &s)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			fmt.Println(s)
+			w.Write([]byte("Good job"))
 		}
-		err = json.Unmarshal(b, &s)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		fmt.Println(s)
-		w.Write([]byte("Good job"))
-	}
 	default:
 		w.Write([]byte("Method Not Allowed"))
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -92,52 +94,51 @@ func unmarshal(w http.ResponseWriter, r *http.Request) {
 
 func encode(w http.ResponseWriter, r *http.Request) {
 	s := Student{
-		Name: "Srikote",
-		Age: 24,
+		Name:  "Srikote",
+		Age:   24,
 		Score: 100.0,
 	}
-    encoder := json.NewEncoder(w)
-    err := encoder.Encode(s)
-    if err != nil {
-    	http.Error(w, err.Error(), http.StatusBadRequest)
-    	return
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 }
 
 func marshal(w http.ResponseWriter, r *http.Request) {
-    s := Student{
-    	Name: "Supanut",
-    	Age: 23,
-    	Score: 1000.0,
+	s := Student{
+		Name:  "Supanut",
+		Age:   23,
+		Score: 1000.0,
 	}
-    encoder := json.NewEncoder(w)
-    err := encoder.Encode(s)
-    if err != nil {
-    	http.Error(w, err.Error(), http.StatusBadRequest)
-    	return
+	encoder := json.NewEncoder(w)
+	err := encoder.Encode(s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 }
 
 func fetchGoogle(w http.ResponseWriter, r *http.Request) {
-    resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
-    //resp, err := http.NewRequest("GET", "https://jsonplaceholder.typicode.com/todos/1", nil)
-    if err != nil {
-    	http.Error(w, err.Error(), http.StatusInternalServerError)
-    	log.Fatal(err.Error())
-    	return
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	//resp, err := http.NewRequest("GET", "https://jsonplaceholder.typicode.com/todos/1", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatal(err.Error())
+		return
 	}
 
-
 	var t Todo
-    decoder := json.NewDecoder(resp.Body)
-    err = decoder.Decode(&t)
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&t)
 
-    encoder := json.NewEncoder(w)
-    encoder.Encode(t)
+	encoder := json.NewEncoder(w)
+	encoder.Encode(t)
 
-    if err != nil {
-    	http.Error(w, err.Error(), http.StatusInternalServerError)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Fatal(err.Error())
-    	return
+		return
 	}
 }
